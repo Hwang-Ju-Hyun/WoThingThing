0d4c8f145210fe513a11ffde5db8ea5ff5390265
Donghan <space12590@gamil.com>
2024-08-26 11:15:06 +0900

    Edit Player movement

--- WoThingThing/TransComponent.cpp
+++ WoThingThing/TransComponent.cpp
@@ -10,56 +10,11 @@ TransComponent::TransComponent(GameObject* _owner)
 	//이 코드들도 나중에 stage01 _init으로 옮겨야함
 	if (_owner->GetName() == "Player1")
 	{
-		m_vPos = { -755,0 };
-		//m_vScale = { 50,300 };
-		m_vScale = { 10,100 };
-		m_fRot = 0.f;
-	}
-	if (_owner->GetName() == "Player2")
-	{
-		m_vPos = { 755,0 };
-		//m_vScale = { 50,300 };
-		m_vScale = { 10,100 };
-		m_fRot = 0.f;
-	}
-	if (_owner->GetName() == "Ball")
-	{
-		m_vPos = { 0,0 };
+		m_vPos = { -755,-380 };
 		m_vScale = { 50,50 };
 		m_fRot = 0.f;
 	}
 
-	if (_owner->GetName() == "GoalPost1")
-	{
-		m_vPos = { -800,0 };
-		m_vScale = { 30,1000 };
-		m_fRot = 0.f;
-	}
-
-	if (_owner->GetName() == "GoalPost2")
-	{
-		m_vPos = { 800,0 };
-		m_vScale = { 30,1000 };
-		m_fRot = 0.f;
-	}
-
-	if (_owner->GetName() == "UpperPost")
-	{
-		s32 window_width=AEGfxGetWindowWidth();
-		s32 window_height = AEGfxGetWindowHeight();
-		m_vPos = { 0,float(window_height/2.f)};
-		m_vScale = { float(window_width),30};
-		m_fRot = 0.f;
-	}
-	if (_owner->GetName() == "DownPost")
-	{
-		s32 window_width = AEGfxGetWindowWidth();
-		s32 window_height = AEGfxGetWindowHeight();
-		m_vPos = {  0,-float(window_height / 2.f)};
-		m_vScale = { float(window_width),30 };
-		m_fRot = 0.f;
-	}
-	
 	if (_owner->GetName() == "Start")
 	{
 		m_vPos = {50,0};
